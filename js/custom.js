16f4db698b0d794fc57bb5acf5a945bba8636797
SUNDOLI <sundoli83@gmail.com>
2025-03-20 09:27:07 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -72,3 +72,31 @@ $(function () {
         $('')
     })
 })
+
+$(function () {
+    $('.header .mbtn').on('click', function () {
+        $(this).toggleClass('on');
+        $('.header .gnb').toggleClass('on');
+        $('.header .my_page').toggleClass('on');
+
+
+    })
+    $('.header .gnb>ul>li>a').on('click', function (e) {
+        if ($('.header .gnb').hasClass('on')) {
+            e.preventDefault();
+            $('.header .gnb>ul ul').slideUp();
+            $(this).next().slideDown();
+        }
+    });
+
+    $(window).on('resize', function () {
+        $('.header .mbtn').removeClass('on')
+        $('.header .gnb').removeClass('on')
+        $('.header .gnb>ul ul').removeAttr('style')
+    })
+
+    $('.header .gnb').on('wheel touchmove', function (e) {
+        e.preventDefault();
+    })
+
+})
